4f87893f184569c0bf80bc135bc8f89fb863a1b2
권태형 <power916@daum.net>
2023-05-01 17:50:06 +0900

    Refactor: synchronize false 처리

--- backend/src/database/database.repository.ts
+++ backend/src/database/database.repository.ts
@@ -19,7 +19,7 @@ export const databaseProviders = [
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
         entities: [User, Topic, Leaf, Code, Follow, Bookmark, Like],
-        synchronize: true,
+        synchronize: false,
       });
 
       return dataSource.initialize();
