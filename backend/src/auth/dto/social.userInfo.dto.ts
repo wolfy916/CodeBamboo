9ecf4a1fd1319af382ebae769b1c784e354fc5f9
chanshin0 <windcj1@naver.com>
2023-05-02 09:12:12 +0900

    Fix : LoginUserDTO auth폴더로 위치 조정

--- backend/src/auth/dto/social.userInfo.dto.ts
+++ backend/src/auth/dto/social.userInfo.dto.ts
@@ -1,5 +1,6 @@
 import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
+// 소셜로그인 provider에게 받아오는 유저인포 DTO
 export class SocialUserInfoDto {
   @IsString()
   @IsNotEmpty()
@@ -22,7 +23,7 @@ export class SocialUserInfoDto {
   provider?: string;
 }
 
-
+// 로그인 로직 전부 완료하고, 컨트롤러로 반환할 때의 DTO
 export class SocialLoginResponseDto {
   refresh_token: string;
   user: {
