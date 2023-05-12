4c0d1542004293c2e8f9280437ec4a603973d1ef
권태형 <power916@daum.net>
2023-05-12 15:50:40 +0900

    Refactor: TopicItem 범용성 확장

--- frontend/components/main/Main.tsx
+++ frontend/components/main/Main.tsx
@@ -2,27 +2,22 @@ import { UserItem } from '@/components/UserItem';
 import useIsMobile from '@/hooks/useIsMobile';
 import useIsClient from '@/hooks/useIsClient';
 import { TopicList } from '../topic/TopicList';
-
-// <<query 전략을 정의한다.>>
-
-// 1. useQuery
-// 1-1. 쿼리 키를 정의한다. 여기에도 일정한 컨벤션을 갖는 편이 좋다.
-// 컨벤션) [api명세의 대분류_리소스 경로, 동적인 인자(쿼리스트링 or {path})]
-
-// 2. useMutaion
-// const queryFn = async () => {
-//   // 로그아웃 하시겠습니까? 로직 추가..
-//   try {
-//     const response = await Authapi.post('auth/logout');
-//     return response.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+import { useEffect } from 'react';
+import { getMainList } from '@/hooks/api/topic.api';
+import { useRecoilState } from 'recoil';
+import { mainTopicListState } from '@/recoil/topic';
 
 export default function Main() {
   const isClient = useIsClient();
   const isMobile = useIsMobile();
+  const [mainTopicList, setMainTopicList] = useRecoilState(mainTopicListState);
+
+  useEffect(() => {
+    if (isClient) {
+      getMainList(setMainTopicList);
+    }
+  }, [isClient]);
+
   return (
     <div className="relative w-full top-[700vh] h-[100vh] overflow-y-scroll z-30 scrollbar-hide">
       {/* 스크롤바에 밀리지않게 더미 박스 매우 중요*/}
@@ -34,7 +29,7 @@ export default function Main() {
       >
         Popular
       </p>
-      <TopicList type={0} />
+      <TopicList topicList={mainTopicList.popular.slice(0, 3)} />
       <p
         className="m-5
                   text-3xl
@@ -42,14 +37,14 @@ export default function Main() {
       >
         Trending
       </p>
-      <TopicList type={1} />
+      <TopicList topicList={mainTopicList.trending.slice(0, 3)} />
       <p
         className="m-5
                   text-3xl
                   md:mx-20 md:mt-7 md:text-5xl"
       >
         Interesting people
-        {isMobile && <br />}
+        {isMobile ? <br /> : ' '}
         to follow
       </p>
       <UserItem />
