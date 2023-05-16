71fd49aebd5b27aa6c3885b9e63f062e7418a224
wonseokjee <wsji9404@gmail.com>
2023-05-16 09:40:09 +0900

    refactor: conflict resolve

--- backend/src/topics/topics.controller.ts
+++ backend/src/topics/topics.controller.ts
@@ -43,7 +43,7 @@ export class TopicsController {
   getOne(@Param('id') id: number, @Req() req: Request) {
     console.log('userId', req.user);
 
-    const user_id = req.user? req.user['user_id'] : 0
+    const user_id = req.user ? req.user['user_id'] : 0;
 
     console.log('user_ID임다', user_id);
     return this.topicsService.getOne(id, user_id);
