868a792443c93e3fa3c78d1b28e138fdd56a931b
윤서용 <swqq31@gmail.com>
2023-05-17 16:34:03 +0900

    Editor 1.41

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <title>Code Bamboo</title>
-        {/* <meta name="name" content="content" /> */}
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
+      <Head />
       <body className="scrollbar-hide">
         <Main />
         <NextScript />
