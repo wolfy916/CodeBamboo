f51fb95bb40435a232b2e4200d936c3cd5425c07
wonseokjee <wsji9404@gmail.com>
2023-05-17 14:05:48 +0900

    chore: mainCSS 변경

--- frontend/components/main/Main.tsx
+++ frontend/components/main/Main.tsx
@@ -7,37 +7,36 @@ import { useRecoilState } from 'recoil';
 import { mainTopicListState } from '@/recoil/topic';
 
 export default function Main() {
-    const isClient = useIsClient();
-    const isMobile = useIsMobile();
-    const [mainTopicList, setMainTopicList] =
-        useRecoilState(mainTopicListState);
+  const isClient = useIsClient();
+  const isMobile = useIsMobile();
+  const [mainTopicList, setMainTopicList] = useRecoilState(mainTopicListState);
 
-    useEffect(() => {
-        if (isClient) {
-            getMainList(setMainTopicList);
-        }
-    }, [isClient]);
+  useEffect(() => {
+    if (isClient) {
+      getMainList(setMainTopicList);
+    }
+  }, [isClient]);
 
-    return (
-        <div className="relative w-full top-[700vh] h-[100vh] overflow-y-scroll z-30 scrollbar-hide">
-            {/* 스크롤바에 밀리지않게 더미 박스 매우 중요*/}
-            {isClient && isMobile && <div className="w-full h-20"></div>}
-            <div
-                className="m-5 mb-0 pl-5 pb-5 border-b-gray-600 border-b-4
+  return (
+    <div className="relative w-full top-[700vh] h-[100vh] overflow-y-scroll z-30 scrollbar-hide">
+      {/* 스크롤바에 밀리지않게 더미 박스 매우 중요*/}
+      {isClient && isMobile && <div className="w-full h-20"></div>}
+      <div
+        className="m-5 mb-0 pl-5 pb-5 border-b-gray-600 border-b-2
                   text-3xl
-                  md:mx-20 md:mt-7 md:text-5xl"
-            >
-                Popular
-            </div>
-            <TopicList topicList={mainTopicList.popular} />
-            <div
-                className="m-5 mb-0 pl-5 pb-5 border-b-gray-600 border-b-4
+                  md:mx-20 md:mt-7 md:text-4xl"
+      >
+        Popular
+      </div>
+      <TopicList topicList={mainTopicList.popular} />
+      <div
+        className="m-5 mb-0 pl-5 pb-5 border-b-gray-600 border-b-2
                   text-3xl
-                  md:mx-20 md:mt-7 md:text-5xl"
-            >
-                Trending
-            </div>
-            <TopicList topicList={mainTopicList.trending} />
-        </div>
-    );
+                  md:mx-20 md:mt-14 md:text-4xl"
+      >
+        Trending
+      </div>
+      <TopicList topicList={mainTopicList.trending} />
+    </div>
+  );
 }
