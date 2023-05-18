695fa402a6d71630fd3411f472fc799b614289c6
SeoYong <swqq31@gmail.com>
2023-05-18 10:37:25 +0900

    Feat: 삭제 및 leaf update rendering

--- frontend/pages/topics/[topicId].tsx
+++ frontend/pages/topics/[topicId].tsx
@@ -13,6 +13,17 @@ import Editor from '@/components/editor/Editor';
 import { Log } from '@/components/editor/Log';
 import authApi from '@/hooks/api/axios.authorization.instance';
 
+export const queryTopicDetailFn = async (topicId: any) => {
+  if (!topicId) return null;
+  try {
+    const response = await authApi.get(`topic/${topicId}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
 interface Props {}
 
 export const TopicDetail = ({}: Props) => {
@@ -23,17 +34,8 @@ export const TopicDetail = ({}: Props) => {
   const setSelectedLeaf = useSetRecoilState(selectedLeafState);
   const setLeafs = useSetRecoilState(LeafState);
 
-  const queryFn = async (topicId: any) => {
-    if (!topicId) return;
-    try {
-      const response = await authApi.get(`topic/${topicId}`);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
-  const getTopic = useQuery(['topic', topicId], () => queryFn(topicId), {
+  const getTopic = useQuery(['topic', topicId], () => queryTopicDetailFn(topicId), {
     onSuccess: (data) => {
       setCode(data?.bestLeaf.codes);
       setArticle({
